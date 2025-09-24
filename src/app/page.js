11f4edecc5a9981443259cb7e6bde62199432437
Mittal kumar gupta <mittalkumargupta@gmail.com>
2025-09-24 15:51:00 +0530

    f11

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,40 @@
 
 import ExpenseTrackerLanding from "../component/landingPage/ExpenseTrackerLanding.jsx";
 import {Button, HeroUIProvider} from "@heroui/react";
-
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { useEffect } from "react";
 export default function Home() {
+
+  const router = useRouter();
+
+    useEffect(() => {
+      const checkAuth = async () => {
+        try {
+          const res = await fetch("/api/auth/me", {
+            method: "GET",
+            credentials: "include", // send cookies
+          });
+  
+          if (res.ok) {
+            const data = await res.json();
+            if (data?.user) {
+              // Redirect based on role
+              if (data.user.role === "admin") router.replace("/Admin");
+              else router.replace("/Dashboard");
+            }
+
+          }
+         
+         
+        } catch (err) {
+          
+        }
+      };
+  
+      checkAuth();
+    }, [router]);
+  
   
   return (
     <div className="  min-h-screen ">
